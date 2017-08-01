db77dd95316773811ba91d396459c8ad462d2b34
Ying Zhou <yingzhou@thoughtworks.com>
2017-08-01 19:07:19 +0800

    make the region editable

--- launcher.py
+++ launcher.py
@@ -4,25 +4,38 @@ from urllib import request
 import appJar
 from bs4 import BeautifulSoup
 
-URL1 = "https://www.amazon.com/gp/search/ref=sr_il_to_mobile?fst=as%3Aon&rh=k%3A"
-URL2 = "%2Cn%3A2335752011%2Cn%3A2407760011%2Cn%3A3081461011&lo=none"
+URL_COM_1 = "https://www.amazon.com/gp/search/ref=sr_il_to_mobile?fst=as%3Aon&rh=k%3A"
+URL_COM_2 = "%2Cn%3A2335752011%2Cn%3A2407760011%2Cn%3A3081461011&lo=none"
+URL_DE = "https://www.amazon.de/s/ref=nb_sb_noss_2?__mk_de_DE=%C3%85M%C3%85%C5%BD%C3%95%C3%91&url=search-alias%3Daps&field-keywords="
+URL_UK = "https://www.amazon.co.uk/s/ref=nb_sb_noss_2/261-4880887-8776658?url=search-alias%3Daps&field-keywords="
+URL_JP = "https://www.amazon.co.jp/s/ref=nb_sb_noss_2/358-9024954-2951136?__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&url=search-alias%3Daps&field-keywords="
 
 
-def get_search_result(key, i):
+def get_search_result(base, key, i):
     key = key.replace(" ", "+")
     page = "&page=" + str(i)
-    url = URL1 + key + URL2 + "&keywords=" + key + page
     headers = {
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
         'Accept-Encoding': 'gzip',
         'Accept-Language': 'en,zh-CN;q=0.8,zh;q=0.6',
         'Cache-Control': 'no-cache',
         'Connection': 'keep-alive',
-        'Host': 'www.amazon.com',
         'Pragma': 'no-cache',
         'Upgrade-Insecure-Requests': '1',
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'
     }
+    if base == "美国":
+        url = URL_COM_1 + key + URL_COM_2 + "&keywords=" + key + page
+        headers['Host'] = 'www.amazon.com'
+    elif base == "德国":
+        url = URL_DE + key + page
+        headers['Host'] = 'www.amazon.de'
+    elif base == "英国":
+        url = URL_UK + key + page
+        headers['Host'] = 'www.amazon.co.uk'
+    else:
+        url = URL_JP + key + page
+        headers['Host'] = 'www.amazon.co.jp'
     my_request = request.Request(url=url, headers=headers)
     res = request.urlopen(my_request, timeout=20)
     html = res.read()
@@ -34,18 +47,18 @@ def get_search_result(key, i):
     if items:
         print(len(items))
         for item in items:
-            if item.find(text="Sponsored"):
-                result += "Sponsored    "
+            if item.h5:
+                result += item.h5.contents[0] + "    "
             else:
                 result += "     "
             result += item.h2.text + '    '
             result += item.h2.find_next("div").text + '    '
-            price = item.select("span.a-offscreen")
-            if price:
-                result += price[0].text + '    '
+            # price = item.select("span.a-offscreen")
+            price = item.h2.find_next("a")
+            if 'aria-label' in price.span.attrs:
+                result += price['aria-label'] + '    '
             else:
-                price = item.select("span['aria-label']")
-                result += price[0]['aria-label'] + '    '
+                result += price.text + '    '
             star = item.select("i.a-icon-star span.a-icon-alt")
             if star:
                 result += star[0].text + '    '
@@ -55,13 +68,14 @@ def get_search_result(key, i):
 
 
 def search_key(app):
+    base = app.getOptionBox("base")
     key = app.getEntry("key")
     page = app.getEntry("page")
     i = int(page)
     get_next = True
-    fl = open(key + '.txt', 'wb')
+    fl = open(key + base + '.txt', 'wb')
     while get_next:
-        message, url = get_search_result(key, i)
+        message, url = get_search_result(base, key, i)
         if message == "":
             app.errorBox("没了", "上一页是最后一页")
             get_next = False
@@ -78,12 +92,14 @@ def main():
 
     app = appJar.gui("晓狗")
     app.setGuiPadding(30, 30)
-    app.addLabel("key", "搜索词", 0, 0, 1, 1)
-    app.addEntry("key", 0, 1, 1, 1)
-    app.addLabel("page", "开始页", 1, 0, 1, 1)
-    app.addNumericEntry("page", 1, 1, 1, 1)
+    app.addLabel("base", "网站", 0, 0, 1, 1)
+    app.addOptionBox("base", ["美国", "德国", "英国", "日本"], 0, 1, 1, 1)
+    app.addLabel("key", "搜索词", 1, 0, 1, 1)
+    app.addEntry("key", 1, 1, 1, 1)
+    app.addLabel("page", "开始页", 2, 0, 1, 1)
+    app.addNumericEntry("page", 2, 1, 1, 1)
     app.setEntry("page", "1")
-    app.addButton("开始", search, 2, 0, 2, 1)
+    app.addButton("开始", search, 3, 0, 2, 1)
     app.go()
 
 
